eb456937c490e2259febabf01c73df808b463243
StephenGregan <grego@live.ie>
2018-05-24 14:33:21 +0100

    initial commit

---
+++ ConsoleAppSetConsoleWindowSize/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSetConsoleWindowSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 40; i++)
+            {
+                Console.SetWindowSize(i, i);
+                System.Threading.Thread.Sleep(50);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
